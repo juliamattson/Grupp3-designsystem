5e3158d8ae465ae02bc73c100b0b3163ff2cfec2
Afshar20922019 <afshar2092@yahoo.com>
2020-05-29 14:55:30 +0200

    Create components

---
+++ my-app/src/components/Product.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class product extends Component {
+    render() {
+        return (
+            <div>
+
+                <h3>Hello from products!!</h3>
+                </div>
+        )
+    }
+}
+
