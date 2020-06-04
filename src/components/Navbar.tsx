610dff4c5846d6ea2855e447fcae55a0e189db51
Anton <antonwelliver27@gmail.com>
2020-06-04 13:36:36 +0200

    fixed store button route in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Navbar } from 'react-bootstrap';
-import Button from 'react-bootstrap/Button';
-
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 
 export default class NavBar extends Component {
     render() {
         return (
             <Navbar bg="dark" className="navbar">
-                <Navbar.Brand href="#home">Store</Navbar.Brand>
+                <Navbar.Brand href="/">Store</Navbar.Brand>
                 <Link to="/" className="nav-link">
                     Products
-                        </Link>
+                </Link>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
                     <Link to="/cart" className="nav-link">
@@ -23,7 +21,6 @@ export default class NavBar extends Component {
                     </Link>
                 </Navbar.Collapse>
             </Navbar>
-        )
+        );
     }
 }
-
