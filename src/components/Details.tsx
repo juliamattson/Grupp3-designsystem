47868ee5b7e9792f40960c232cb7c817571b8693
Afshar20922019 <afshar2092@yahoo.com>
2020-06-04 14:32:47 +0200

    Details fixas

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,40 +1,37 @@
 import React, { Component } from "react";
 import { NavLink, Route, RouteComponentProps } from "react-router-dom";
-import {storeProducts} from '../data';
-interface Props extends RouteComponentProps<{id: string}>{
+import { storeProducts } from '../data';
+interface Props extends RouteComponentProps<{ id: string }> {
 
 }
-
-
-
 export default class Details extends Component<Props> {
   render() {
     this.props.match.params.id
     return (
-           
-                     <div className="Container">
-              <div className="ImgContainer">
-                <img src={img} alt={title} className="Img" />
-              </div>
-              <div className="InfoContainer">
-                <h1 className="Title">{title}</h1>
-                <h3>Made by: {company}</h3>
-                <p>
-                  <strong>Description: </strong>
-                  {info}
-                </p>
-                <h2>Price: £ {price}</h2>
-                <div className="BtnContainer">
-                  <NavLink to="/shop">
-                    <button className="BackButton">Back to Shop</button>
-                  </NavLink>
-                  
-                </div>
-              </div>
-            </div>
-          );
-       
-    
-  
-}
+
+      <div className="Container">
+        <div className="ImgContainer">
+          <img src={img} alt={title} className="Img" />
+        </div>
+        <div className="InfoContainer">
+          <h1 className="Title">{title}</h1>
+          <h3>Made by: {company}</h3>
+          <p>
+            <strong>Description: </strong>
+            {info}
+          </p>
+          <h2>Price: £ {price}</h2>
+          <div className="BtnContainer">
+            <NavLink to="/shop">
+              <button className="BackButton">Back to Shop</button>
+            </NavLink>
+
+          </div>
+        </div>
+      </div>
+    );
+
+
+
+  }
 }
