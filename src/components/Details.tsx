ca0517431f29feb9fbdd642687184929b5792180
Afshar20922019 <afshar2092@yahoo.com>
2020-06-06 15:28:34 +0200

    fixas details sida

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,35 +1,53 @@
 import React, { Component } from "react";
 import { NavLink, Route, RouteComponentProps } from "react-router-dom";
 import { storeProducts } from '../data';
-interface Props extends RouteComponentProps<{ id: string }> {
+import product from "./Product";
 
-}
+
+interface Props extends RouteComponentProps<{ id: string }> { }
+
+
 export default class Details extends Component<Props> {
   render() {
-    this.props.match.params.id
-    return (
+    this.props.match.params.id;
+    const foundProduct = storeProducts.find((product) => {
 
-      <div className="Container">
-        <div className="ImgContainer">
-          <img src={img} alt={title} className="Img" />
-        </div>
-        <div className="InfoContainer">
-          <h1 className="Title">{title}</h1>
-          <h3>Made by: {company}</h3>
-          <p>
-            <strong>Description: </strong>
-            {info}
-          </p>
-          <h2>Price: £ {price}</h2>
-          <div className="BtnContainer">
-            <NavLink to="/shop">
-              <button className="BackButton">Back to Shop</button>
-            </NavLink>
+      return product.id == Number(this.props.match.params.id);
 
+
+    });
+
+    if (foundProduct) {
+      return (
+
+        <div className="Container">
+          <div className="ImgContainer">
+            <img src={foundProduct.img} alt={foundProduct.title} className="Img" />
+          </div>
+          <div className="InfoContainer">
+            <h1 className="Title">{foundProduct.title}</h1>
+            <h3>Made by: {foundProduct.company}</h3>
+            <p>
+              <strong>Description: </strong>
+              {foundProduct.info}
+            </p>
+            <h2>Price: £ {foundProduct.price}</h2>
+            <div className="BtnContainer">
+              <NavLink to="/shop">
+                <button className="BackButton">Back to Shop</button>
+              </NavLink>
+
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+
+    } else {
+
+
+    }
+
+
 
 
 
