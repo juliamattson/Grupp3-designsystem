0c5892cec5481ed6621b205764231ef91d9aaf9f
Afshar20922019 <afshar2092@yahoo.com>
2020-06-08 14:11:36 +0200

    Skapas radio buton kort-swish

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -12,62 +12,88 @@ import CardGroup from 'react-bootstrap/CardGroup'
 export default class Cart extends Component {
     render() {
         return (
-          <div id="cartStyling">
-          <CardGroup>
-          <Card>
-            <Card.Body>
-              <Card.Title>Dina uppgifter</Card.Title>
-              <Form>
-              <Form.Text className="text-muted">
-                  Fyll i dina uppgifter nedan.
+            <div id="cartStyling">
+                <CardGroup>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Dina uppgifter</Card.Title>
+                            <Form>
+                                <Form.Text className="text-muted">
+                                    Fyll i dina uppgifter nedan.
                 </Form.Text>
-                <hr/>
-              <Form.Group controlId="formBasicName">
-                <Form.Control autoComplete="on" type="text" placeholder="För och efternamn" />
-              </Form.Group>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Control autoComplete="on" type="email" placeholder="Email-adress" />
-              </Form.Group>
-              <Form.Group controlId="formBasicNumber">
-                <Form.Control autoComplete="on" type="text" placeholder="Telefonnummer" />
-              </Form.Group>
-              <Form.Group controlId="formBasicAdress">
-                <Form.Control autoComplete="on" type="text" placeholder="Adress" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Spara
+                                <hr />
+                                <Form.Group controlId="formBasicName">
+                                    <Form.Control autoComplete="on" type="text" placeholder="För och efternamn" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Control autoComplete="on" type="email" placeholder="Email-adress" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicNumber">
+                                    <Form.Control autoComplete="on" type="text" placeholder="Telefonnummer" />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicAdress">
+                                    <Form.Control autoComplete="on" type="text" placeholder="Adress" />
+                                </Form.Group>
+                                <Button variant="primary" type="submit">
+                                    Spara
               </Button>
-            </Form>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Body>
-              <Card.Title>Betalsätt</Card.Title>
-              <Card.Text>
-                Här ska info och alternativ av betalsätt finnas
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Betalsätt</Card.Title>
+                            <Card.Text>
+                                Här ska info och alternativ av betalsätt finnas
+                <Form>
+                                    {['radio'].map((type) => (
+                                        <div key={`default-${type}`} className="mb-3">
+                                            <Form.Check
+                                                type="radio"
+                                                label="kort "
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios1"
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                label="Swish "
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios2"
+                                            />
+                                            <Form.Check
+                                                type="radio"
+                                                label="Pay Pal"
+                                                name="formHorizontalRadios"
+                                                id="formHorizontalRadios3"
+                                            />
+
+
+                                        </div>
+                                    ))}
+                                </Form>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Fraktsätt</Card.Title>
+                            <Card.Text>
+                                Här ska info och alternativ av fraktsätt finnas
               </Card.Text>
-            </Card.Body>
-          </Card>
-          <Card>
-            <Card.Body>
-              <Card.Title>Fraktsätt</Card.Title>
-              <Card.Text>
-                Här ska info och alternativ av fraktsätt finnas
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </CardGroup>
-        <Card>
-        <Card.Body>
-          <Card.Title>Kundvagn</Card.Title>
-          <Card.Text>
-            Om det finns några produkter i kundvagnen ska de visas här.
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Kundvagn</Card.Title>
+                        <Card.Text>
+                            Om det finns några produkter i kundvagnen ska de visas här.
           </Card.Text>
-          <Button variant="primary">Slutför köp</Button>
-        </Card.Body>
-        </Card>
-        </div>
-         
+                        <Button variant="primary">Slutför köp</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
         )
     }
 }
