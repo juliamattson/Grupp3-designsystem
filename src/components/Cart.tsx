e38484cfafe0ff6b1c78b4a48bde09fbaa45b0df
juliamattson <julia.mattson@medieinstitutet.se>
2020-06-08 12:13:55 +0200

    Added cards on the cart-page and a form

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -5,33 +5,70 @@ import Dropdown from 'react-bootstrap/Dropdown'
 import Card from 'react-bootstrap/Card'
 import Nav from 'react-bootstrap/Nav'
 import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form'
+import "./cart.css";
+import CardGroup from 'react-bootstrap/CardGroup'
 
 export default class Cart extends Component {
     render() {
         return (
-            <Card>
-            <Card.Header>
-              <Nav variant="tabs" className="justify-content-center"  defaultActiveKey="#first">
-                <Nav.Item>
-                  <Nav.Link href="#first">Dina uppgifter</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#second">Fraktsätt</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link href="#third">Betalsätt</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Card.Header>
+          <div id="cartStyling">
+          {/* <input autoComplete={true} /> */}
+          <CardGroup>
+          <Card>
             <Card.Body>
-              <Card.Title>Din kundvagn</Card.Title>
-              <Card.Text>
-                Här ska produkterna synas om det finns några i kundvagnen
-              </Card.Text>
-              <Button variant="primary">Slutför köp</Button>
+              <Card.Title>Dina uppgifter</Card.Title>
+              <Form>
+              <Form.Text className="text-muted">
+                  Fyll i dina uppgifter nedan.
+                </Form.Text>
+                <hr/>
+              <Form.Group controlId="formBasicName">
+                <Form.Control type="email" placeholder="För och efternamn" />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Email-adress" />
+              </Form.Group>
+              <Form.Group controlId="formBasicNumber">
+                <Form.Control type="email" placeholder="Telefonnummer" />
+              </Form.Group>
+              <Form.Group controlId="formBasicAdress">
+                <Form.Control type="email" placeholder="Adress" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Spara
+              </Button>
+            </Form>
             </Card.Body>
           </Card>
-
+          <Card>
+            <Card.Body>
+              <Card.Title>Betalsätt</Card.Title>
+              <Card.Text>
+                Här ska info och alternativ av betalsätt finnas
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card>
+            <Card.Body>
+              <Card.Title>Fraktsätt</Card.Title>
+              <Card.Text>
+                Här ska info och alternativ av fraktsätt finnas
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+        <Card>
+        <Card.Body>
+          <Card.Title>Kundvagn</Card.Title>
+          <Card.Text>
+            Om det finns några produkter i kundvagnen ska de visas här.
+          </Card.Text>
+          <Button variant="primary">Slutför köp</Button>
+        </Card.Body>
+        </Card>
+        </div>
+         
         )
     }
 }
