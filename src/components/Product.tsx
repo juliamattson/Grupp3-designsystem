35a06ebe999fa8aeb8bb95a178873367f6088d34
Afshar20922019 <afshar2092@yahoo.com>
2020-06-08 10:36:25 +0200

    css fix columns

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -18,46 +18,44 @@ export default class product extends Component<props, State> {
     render() {
         const { id, title, img, price, inCart } = this.props.product;
         return (
-                <Row className="product-row">
-                    <Col>
-                        <Card style={{ width: '18rem' }}>
-                            <Link to={"/details/"+id}>
-                                <Card.Img variant="top" src={img} alt="product"
-                                    onClick={() => console.log("You clicked me on the image container!")} />
-                            </Link>
-                            <Button
-                                className="cart-btn"
-                                disabled={inCart ? true : false}
-                                onClick={() => {
-                                    console.log("Added to cart!!!");
-                                }}
-                            >
-                                {inCart ? (
-                                    <p className="text-capitalize mb-0" >
-                                        {" "}
+            <Row className="product-row">
+                <Col>
+                    <Card style={{ width: '16rem' }}>
+                        <Link to={"/details/" + id}>
+                            <Card.Img variant="top" src={img} alt="product"
+                                onClick={() => console.log("You clicked me on the image container!")} />
+                        </Link>
+                        <Button
+                            className="cart-btn"
+                            disabled={inCart ? true : false}
+                            onClick={() => {
+                                console.log("Added to cart!!!");
+                            }}
+                        >
+                            {inCart ? (
+                                <p className="text-capitalize mb-0" >
+                                    {" "}
                                 in inCart
-                                    </p>
-                                ) : (
-                                        <i className="fas fa-cart-plus" />
-                                    )}
-                            </Button>{' '}
-                            {/*cart footer*/}
+                                </p>
+                            ) : (
+                                    <i className="fas fa-cart-plus" />
+                                )}
+                        </Button>{' '}
+                        {/*cart footer*/}
                         <Row className="card-footer d-flex justify-content-between">
                             <Col>
                                 <p className="align-self-center mb-0">{title}</p>
                                 <h4 className="text-blue font-italic mb-0">
-                                    
+
                                     {price}
-                                    <span>
-                                        kr
-                                        </span>
+                                    <span>kr</span>
                                 </h4>
                             </Col>
                         </Row>
-                        </Card>
-                        
-                    </Col>
-                </Row>
+                    </Card>
+
+                </Col>
+            </Row>
         )
     }
 }
