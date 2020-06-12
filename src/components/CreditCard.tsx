dd28d4ec99667aa3284ea720de06983417e2b440
Anton <antonwelliver27@gmail.com>
2020-06-12 11:45:50 +0200

    validation fix

--- src/components/CreditCard.tsx
+++ src/components/CreditCard.tsx
@@ -1,47 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Form from "react-bootstrap/Form";
 
-
-export  class creditCard extends Component  {
-   
-    handleChange=(  e: React.ChangeEvent<HTMLInputElement>)=> {
+export class creditCard extends Component {
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.currentTarget;
-        this.setState({
-           
-             ...this.state,
-             [ name]: value
-          
-          }, ()=>{console.log(this.state)});  
+        this.setState(
+            {
+                ...this.state,
+                [name]: value,
+            },
+            () => {
+                console.log(this.state);
+            }
+        );
     };
     render() {
         return (
-        <Form.Group  >
-        <Form.Control 
-         placeholder="Card Holder"
-         type="text"
-            />
-            <Form.Control 
-          placeholder="Card Number"
-          name='cardNumber' 
-          onChange={this.handleChange}
-              />
-           <Form.Control 
-         placeholder="Expires"
-         type="month"
-         defaultValue="2020-05" 
-            />
-        <Form.Control 
-         placeholder="CVC/CVV"
-         defaultValue="000" 
-           />
-                
-              </Form.Group>
-        )
+            <Form.Group>
+                <Form.Control required placeholder="Card Holder" type="text" />
+                <Form.Control
+                    required
+                    placeholder="Card Number"
+                    name="cardNumber"
+                    onChange={this.handleChange}
+                />
+                <Form.Control required placeholder="Expires" type="month" />
+                <Form.Control required placeholder="CVC/CVV" />
+            </Form.Group>
+        );
     }
 }
-
-
-
-
 
 export default creditCard;
--- src/components/SwishForm.tsx
+++ src/components/SwishForm.tsx
@@ -1,20 +1,12 @@
-import React from 'react';
+import React from "react";
 import Form from "react-bootstrap/Form";
 
-
-
-
 export default class SwishForm extends React.Component {
- 
-  render() {
-    return (
-     
-    <Form.Group>
-    <Form.Control placeholder="Telefon" type="text" />
-    </Form.Group>
-       
-    )
-}
-
-
+    render() {
+        return (
+            <Form.Group>
+                <Form.Control required placeholder="Telefon" type="text" />
+            </Form.Group>
+        );
+    }
 }
